07463ef5b3070e0ae3701dffe27a939ea15572c9
monomooncodes <125437441+monomooncodes@users.noreply.github.com>
2023-10-07 11:06:14 +0545

    Create generating.java

---
+++ generating.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+ public class generating{
+   public static void main(static[]  args){
+int length= 18;
+     String genetate_password = genetate_passowrd(length);
+   }
+   public static String generate_password (len){
+     String le= "A-Za-b!@#$%^&*"
+
+       Random rnd = new Random();
+       Scanner s = new Scanner();
+     System.out.printin( "Enter the length of password:");
+     String password = generate_password(size);
+        // printing the password
+        System.out.println(password);
+     
+    
+
+       
