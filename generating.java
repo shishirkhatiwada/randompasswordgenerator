191189b63cbdee9e7ee4bcaf96116a24483e3678
monomooncodes <125437441+monomooncodes@users.noreply.github.com>
2023-10-07 11:19:36 +0545

    Update generating.java

--- generating.java
+++ generating.java
@@ -1,20 +1,36 @@
 import java.util.Random;
 import java.util.Scanner;
- public class generating{
-   public static void main(static[]  args){
-int length= 18;
-     String genetate_password = genetate_passowrd(length);
-   }
-   public static String generate_password (len){
-     String le= "A-Za-b!@#$%^&*"
 
-       Random rnd = new Random();
-       Scanner s = new Scanner();
-     System.out.printin( "Enter the length of password:");
-     String password = generate_password(size);
-        // printing the password
+public class generator {
+    // function to generate password
+    static String generate_password(int size) {
+        // collection of characters that can be used in password
+        String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-/.,<>?;':\"[]{}\\|`~";
+
+        String password = "";
+        // creating object of Random class
+        Random rnd = new Random();
+        // looping to generate password
+        while (password.length() < size) {
+            // get a random number between 0 and length of chars
+            int index = (int) (rnd.nextFloat() * chars.length());
+            // add character at index to password
+            password += chars.charAt(index);
+        }
+        return password;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of password: ");
+        int size = sc.nextInt();
+        sc.close();
+
+        String password = generate_password(size);
+
         System.out.println(password);
-     
+    }
+}
     
 
        
